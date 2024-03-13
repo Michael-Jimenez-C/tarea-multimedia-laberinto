36a9cf22982a7a062a1c5efa6813c5ec740c71c4
Michael Jimenez <72152481+Michael-Jimenez-C@users.noreply.github.com>
2024-03-12 22:51:22 -0500

    -

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 public class Movement : MonoBehaviour
 {
     public float speed = 5f;
@@ -13,15 +15,34 @@ public class Movement : MonoBehaviour
     Vector3 posicionInicial;
     public Text Monedas;
     public Text Vidas;
+    public int index_escena;
+
 
     private void Start()
     {
-        Cursor.visible = false;
+	int monedas_temp = PlayerPrefs.GetInt("Monedas"); 
+        if(monedas_temp>monedas){    
+                monedas=monedas_temp;
+                Monedas.text = "Monedas: "+monedas;
+        }
+	
+        int vidas_temp = PlayerPrefs.GetInt("Vidas");
+        if(vidas_temp!=vidas && vidas_temp!=0){    
+                vidas=vidas_temp;
+                Vidas.text = "Vidas: "+vidas;
+        }
+
+        PlayerPrefs.SetInt("Monedas",0);
+        PlayerPrefs.SetInt("Vidas",3);
+
+
+	Cursor.visible = false;
         if (lockedCursor){
             Cursor.lockState = CursorLockMode.Locked;
         }
 	miRigidBody = GetComponent<Rigidbody> ();
 	posicionInicial = transform.position;
+	//Debug.Log(posicionInicial);
     }
 
     void Update()
@@ -42,11 +63,20 @@ public class Movement : MonoBehaviour
 
     void OnTriggerEnter(Collider colision){
 	if (colision.CompareTag("Salida")){
-		Debug.Log("Haz encontrado la salida");
+		//Debug.Log("Haz encontrado la salida");
+		PlayerPrefs.SetInt("Monedas",monedas);
+		PlayerPrefs.SetInt("Vidas",vidas);
+		//Debug.Log("Cargando");
+		SceneManager.LoadSceneAsync(index_escena, LoadSceneMode.Single);
 	}else if (colision.CompareTag("Enemigo")){
-		miRigidBody.MovePosition (posicionInicial);
+		miRigidBody.MovePosition(posicionInicial);
 		vidas = vidas - 1;
 		Vidas.text = "Vidas: "+vidas;
+		if (vidas <=0){
+		    PlayerPrefs.SetInt("Monedas",0);
+                    PlayerPrefs.SetInt("Vidas",3);
+	            SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);	
+		}
 	}else if (colision.CompareTag("Moneda")){
 		colision.gameObject.SetActive(false);
 		monedas = monedas + 1;
@@ -55,4 +85,5 @@ public class Movement : MonoBehaviour
 		//Debug.Log(monedas);
 	}
     }
+
 }
--- Assets/Scripts/siguiente_scene.cs
+++ Assets/Scripts/siguiente_scene.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotador : MonoBehaviour
+public class siguiente_scene : MonoBehaviour
 {
-    public float velocidad = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +13,6 @@ public class Rotador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0,velocidad*Time.deltaTime,0);
+        
     }
 }
